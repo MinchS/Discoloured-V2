e2b5e1f252727d13f1a63d7b5b5b723c832a30d3
Me <38901351+GowlD@users.noreply.github.com>
2018-05-15 12:48:45 +1000

    Initial setup
    Game structure and main character sprite

---
+++ js/game.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+
+//Create a new Phaser game, with dimentions of 800px wide and 600px tall
+  var game= new phaser.game(800, 600, phasar.AUTO, 'game-world', {preload: preload, create: create, update: update })
+
+//Preload function, where we can load all the assets that will be used
+  function preload(){
+	  game.load.spritesheet('player', 'assets/character.png', 25, 36);
+
+  }
+
+//Create function, where all the initial objects are created
+  function create(){
+	game.add.sprite(0,0,'player');
+  }
+
+//Update function, runs every frame
+  function update(){
+
+  }
+};
