f28328237da4e24632070ac250dbd275b4a2a740
Mikayla Fitzpatrick <38897134+Fitzpamc@users.noreply.github.com>
2018-05-18 09:50:55 +1000

    Update game.js

--- js/game.js
+++ js/game.js
@@ -3,7 +3,8 @@ window.onload = function() {
 //Create a new Phaser game, with dimentions of 800px wide and 600px tall
   var game= new phaser.game(800, 600, phaser.AUTO, 'game-world', {preload: preload, create: create, update: update })
   
-  var 
+  var bg1c, bg2c;
+  var bg1bw, bg2bw;
 //Preload function, where we can load all the assets that will be used
   function preload(){
 	  game.load.spritesheet('player', 'assets/character.png', 25, 36);
@@ -13,6 +14,16 @@ window.onload = function() {
 
 //Create function, where all the initial objects are created
   function create(){
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+	  
+	  bg1c = game.add.sprite(0,0,'bg');
+	  bg2c = game.add.sprite(game.width,0,'bg');
+	  
+         game.physics.arcade.enable(bg1);
+		bg1c.body.velocity.x = -20;
+		game.physics.arcade.enable(bg2);
+		bg2c.body.velocity.x = -20;
+	  
 	game.add.sprite(0,0,'player');
 	player.body.collideWorldBounds = true;
         controls = game.input.keyboard.addKeys(
@@ -43,8 +54,11 @@ if(controls.left.isDown){
     player.body.velocity.y = -350;
 	  
   function crouch () { //crouching function
-	
 	  
+  if(bg1.x < -game.width) {
+     bg1.x = game.width;
+     } else if (bg2.x < -game.width){
+     bg2.x = game.width;} 
   }
 } //end of update
 
